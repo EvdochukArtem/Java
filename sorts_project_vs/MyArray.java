fdc193dcdc2dcdb09e515ea23958e67c7577b9b3
RaspberryW <evdochukartem@gmail.com>
2020-02-26 18:21:52 +0300

    Refactored all sorts

--- sorts_project_vs/MyArray.java
+++ sorts_project_vs/MyArray.java
@@ -1,5 +1,3 @@
-package simple_Sorts;
-
 class MyArray //implements Cloneable {
 {
     int arr[];
--- sorts_project_vs/SortApp.java
+++ sorts_project_vs/SortApp.java
@@ -1,70 +1,65 @@
-package simple_Sorts;
+import simple.*;
 
 class SortApp {
-    static MyArray arrToSort1;
-    static MyArray arrToSort2;
+    static MyArray arrToSort;
     static MyArray arrUnsortedCopy;
     public static void main(String[] args) {
 
-        int elemNum = 1000000;
-        fillArrays(elemNum/10000);
+        int elemNum = 100000;
+        fillArrays(elemNum/100);
+        long i = SelectionSort.sort(arrToSort.arr);
+        arrToSort.arrayCompare(arrUnsortedCopy.arr);
+        System.out.println(i);
+
+        /*double[] bubleSort = new double[elemNum + 1];
         double[] insertSort = new double[elemNum + 1];
-        double[] insertSort1 = new double[elemNum + 1];
         double[] trueInsertSort = new double[elemNum + 1];
         double[] selectionSort = new double[elemNum + 1];
-        double[] selectionSort1 = new double[elemNum + 1];
-        double[] selectionSort2 = new double[elemNum + 1];
         double[] trueSelectionSort = new double[elemNum + 1];
 
         for (int i = 0; i < elemNum; i++)
         {   
-            insertSort[i] = InsertSort.sort(arrToSort1.arr);
-            System.arraycopy(arrUnsortedCopy.arr, 0, arrToSort1.arr, 0, arrUnsortedCopy.getCurrSize());
-            insertSort1[i] = InsertSort.sort1(arrToSort1.arr);
-            trueInsertSort[i] = TrueInsertSort.sort(arrToSort2.arr);
-            //arrToSort1.arrayCompare(arrToSort2.arr);
-
-            System.arraycopy(arrUnsortedCopy.arr, 0, arrToSort1.arr, 0, arrUnsortedCopy.getCurrSize());
-            System.arraycopy(arrUnsortedCopy.arr, 0, arrToSort2.arr, 0, arrUnsortedCopy.getCurrSize());
-            
-            selectionSort[i] = SelectionSort.sort(arrToSort1.arr);
-            System.arraycopy(arrUnsortedCopy.arr, 0, arrToSort1.arr, 0, arrUnsortedCopy.getCurrSize());
-            selectionSort1[i] = SelectionSort.sort1(arrUnsortedCopy.arr);
-            selectionSort2[i] = SelectionSort.sort2(arrToSort1.arr);
-            trueSelectionSort[i] = TrueSelectionSort.sort(arrToSort2.arr);
-            //arrToSort1.arrayCompare(arrToSort2.arr);
+            bubleSort[i] = BubbleSort.sort(arrToSort.arr);
+            unsortArray();
+            insertSort[i] = InsertSort.sort(arrToSort.arr);
+            unsortArray();
+            trueInsertSort[i] = InsertSort.sortT(arrToSort.arr);
+            unsortArray();
+            selectionSort[i] = SelectionSort.sort(arrToSort.arr);
+            unsortArray();
+            trueSelectionSort[i] = TrueSelectionSort.sort(arrToSort.arr);
         }
         
         for (int i = 0; i < elemNum; i++)
         {
+            bubleSort[elemNum] += bubleSort[i];
             insertSort[elemNum] += insertSort[i];
-            insertSort1[elemNum] += insertSort1[i];
             trueInsertSort[elemNum] += trueInsertSort[i];
             selectionSort[elemNum] += selectionSort[i];
-            selectionSort1[elemNum] += selectionSort1[i];
-            selectionSort2[elemNum] += selectionSort2[i];
             trueSelectionSort[elemNum] += trueSelectionSort[i];
         }
+        System.out.println("Bubble sort:\t\t" + bubleSort[elemNum]/elemNum);
+        System.out.println("");
         System.out.println("Insert Sort:\t\t" + insertSort[elemNum]/elemNum);
-        System.out.println("Insert Sort1:\t\t" + insertSort1[elemNum]/elemNum);
         System.out.println("True Insert Sort:\t" + trueInsertSort[elemNum]/elemNum);
         System.out.println("");
         System.out.println("Selection Sort:\t\t" + selectionSort[elemNum]/elemNum);
-        System.out.println("Selection Sort1:\t" + selectionSort1[elemNum]/elemNum);
-        System.out.println("Selection Sort2:\t" + selectionSort2[elemNum]/elemNum);
-        System.out.println("True Selection Sort:\t" + trueSelectionSort[elemNum]/elemNum);
+        System.out.println("True Selection Sort:\t" + trueSelectionSort[elemNum]/elemNum);*/
     }
 
-    static void fillArrays(int num)
+    private static void fillArrays(int num)
     {
-        arrToSort1 = new MyArray(num);
-        arrToSort2 = new MyArray(num);
+        arrToSort = new MyArray(num);
         arrUnsortedCopy = new MyArray(num);
-        arrToSort1.fillArr(num);
-        System.arraycopy(arrToSort1.arr, 0, arrToSort2.arr, 0, arrToSort1.getCurrSize());
-        System.arraycopy(arrToSort1.arr, 0, arrUnsortedCopy.arr, 0, arrToSort1.getCurrSize());
+        arrUnsortedCopy.fillArr(num);
+        unsortArray();
         System.out.println("Unsorted array:");
-        arrToSort1.showArr();
+        arrUnsortedCopy.showArr();
         System.out.println("--------------------------------------------------------------------");        
+    }
+
+    private static void unsortArray()
+    {
+        System.arraycopy(arrUnsortedCopy.arr, 0, arrToSort.arr, 0, arrUnsortedCopy.getCurrSize());
     }
 }
